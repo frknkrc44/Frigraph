f12f72dd96fbf8345dfadae39c112adfec69a30c
frknkrc44 <krc440002@gmail.com>
2023-07-18 05:03:03 +0300

    Fix delete song dialog for multiple selection

--- app/src/main/java/com/kabouzeid/gramophone/helper/menu/SongsMenuHelper.java
+++ app/src/main/java/com/kabouzeid/gramophone/helper/menu/SongsMenuHelper.java
@@ -1,5 +1,7 @@
 package com.kabouzeid.gramophone.helper.menu;
 
+import android.os.Build;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
@@ -7,6 +9,7 @@ import com.kabouzeid.gramophone.dialogs.AddToPlaylistDialog;
 import com.kabouzeid.gramophone.dialogs.DeleteSongsDialog;
 import com.kabouzeid.gramophone.helper.MusicPlayerRemote;
 import com.kabouzeid.gramophone.model.Song;
+import com.kabouzeid.gramophone.util.MusicUtil;
 
 import org.frknkrc44.frigraph.R;
 
@@ -28,7 +31,11 @@ public class SongsMenuHelper {
                 AddToPlaylistDialog.create(songs).show(activity.getSupportFragmentManager(), "ADD_PLAYLIST");
                 return true;
             case R.id.action_delete_from_device:
-                DeleteSongsDialog.create(songs).show(activity.getSupportFragmentManager(), "DELETE_SONGS");
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+                    DeleteSongsDialog.create(songs).show(activity.getSupportFragmentManager(), "DELETE_SONGS");
+                } else {
+                    MusicUtil.deleteTracks(activity, songs);
+                }
                 return true;
         }
         return false;
